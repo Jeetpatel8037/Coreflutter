a8bbd62453dd29aace51c04bcffcf42ba4f86170
Jeetpatel8037 <111674552+Jeetpatel8037@users.noreply.github.com>
2022-08-22 09:08:07 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,90 +3,206 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: const Text("🛍️ List of Fruits"),
+          title: const Text(
+            "Red & White",
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+          backgroundColor: Colors.red,
           centerTitle: true,
-          backgroundColor: Colors.teal,
+//leading: Icon(Icons.menu),
         ),
-        body: const Center(
-          child: Text.rich(
-            TextSpan(
-              text: "🍎 Apple\n\n",
-              style: TextStyle(
-                fontSize: 35,
-                color: Colors.red,
+        body: Center(
+          child: RichText(
+            text: TextSpan(
+              text: "            G",
+              style: const TextStyle(
+                color: Colors.green,
+                fontSize: 25,
+                letterSpacing: 2,
               ),
               children: [
-                TextSpan(
-                  text: "🍇 Greps\n\n",
+                const TextSpan(
+                  text: "R",
                   style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.deepPurpleAccent,
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
                   ),
-                  children: [
-                    TextSpan(
-                      text: "🍒 Cherry\n\n",
-                      style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.purple,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: "🍓 Strawberry\n\n",
-                          style: TextStyle(
-                            fontSize: 35,
-                            color: Colors.pink,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: "🥭 Mango\n\n",
-                              style: TextStyle(
-                                fontSize: 35,
-                                color: Colors.amber,
-                              ),
-                              children: [
-                                TextSpan(
-                                  text: "🍍 Pineapple\n\n",
-                                  style: TextStyle(
-                                    fontSize: 35,
-                                    color: Colors.green,
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: "🍋 Lemon\n\n",
-                                      style: TextStyle(
-                                        fontSize: 35,
-                                        color: Colors.green,
-                                      ),
-                                      children: [
-                                        TextSpan(
-                                          text: "🍉 Watermelon\n\n",
-                                          style: TextStyle(
-                                            fontSize: 35,
-                                            color: Colors.lightGreenAccent,
-                                          ),
-                                          children: [
-                                            TextSpan(
-                                              text: "🥥 Coconut\n\n",
-                                              style: TextStyle(
-                                                fontSize: 35,
-                                                color: Colors.brown,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                ),
+                const TextSpan(
+                  text: "APHICS",
+                  style: TextStyle(
+                    color: Colors.green,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                TextSpan(
+                  text: "\n    FLUTT",
+                  style: TextStyle(
+                    color: Colors.blue.shade700,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "E",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                TextSpan(
+                  text: "R",
+                  style: TextStyle(
+                    color: Colors.blue.shade700,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\n         AN",
+                  style: TextStyle(
+                    color: Colors.green,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "D",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "ROID",
+                  style: TextStyle(
+                    color: Colors.green,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\nDESIGN",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: " & ",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "DEVELOP",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\n           W",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                TextSpan(
+                  text: "EB",
+                  style: TextStyle(
+                    color: Colors.blue.shade700,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\n       FAS",
+                  style: TextStyle(
+                    color: Colors.amberAccent,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "H",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "ION",
+                  style: TextStyle(
+                    color: Colors.amberAccent,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\n ANIMAT",
+                  style: TextStyle(
+                    color: Colors.teal,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "I",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "ON",
+                  style: TextStyle(
+                    color: Colors.teal,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                TextSpan(
+                  text: "\n            I",
+                  style: TextStyle(
+                    color: Colors.blue.shade700,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "T",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "A - cs+",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\n      GAM",
+                  style: TextStyle(
+                    color: Colors.orange,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                const TextSpan(
+                  text: "E",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    decoration: TextDecoration.none,
+                  ),
                 ),
               ],
             ),
