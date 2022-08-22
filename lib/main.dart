ff97dd8efe71df01e34d04e327486bf24f899549
Jeetpatel8037 <111674552+Jeetpatel8037@users.noreply.github.com>
2022-08-22 11:11:37 +0530

    Add files via upload
    colour pallete

--- lib/main.dart
+++ lib/main.dart
@@ -6,174 +6,120 @@ void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OTP(),
+      home: ColorPallet(),
     ),
   );
 }
 
-class OTP extends StatefulWidget {
-  const OTP({Key? key}) : super(key: key);
+class ColorPallet extends StatefulWidget {
+  const ColorPallet({Key? key}) : super(key: key);
   @override
-  _ProjectsState createState() => _ProjectsState();
+  _ColorPalletState createState() => _ColorPalletState();
 }
 
-class _ProjectsState extends State<OTP> {
-  Random r1 = Random();
-  int length = 0;
-  List l1 = ["0", "0", "0", "0", "0", "0"];
-  TextEditingController lengthControler = TextEditingController();
+class _ColorPalletState extends State<ColorPallet> {
   @override
+  Random r1 = Random();
+  List colors = [
+    Colors.black,
+    Colors.orange,
+    Colors.amber,
+    Colors.indigo,
+    Colors.red,
+    Colors.grey,
+    Colors.blue,
+    Colors.purple,
+    Colors.green,
+    Colors.yellow,
+    Colors.pink,
+    Colors.black12,
+    Colors.teal,
+    Colors.cyan,
+    Colors.deepOrange,
+    Colors.blueGrey,
+    Colors.yellowAccent,
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Stack(
         children: [
-          SizedBox(
+          Container(
+            height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-          ),
-          const Text(
-            "OTP Generator",
-            style: TextStyle(
-                color: Color(0xFFF6DB87),
-                fontSize: 35,
-                fontWeight: FontWeight.w400),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.65,
             decoration: const BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(color: Color(0xFFFCF6BA), width: 1.5)),
-            ),
-            child: TextField(
-              controller: lengthControler,
-              keyboardType: TextInputType.number,
-              maxLength: 1,
-              onChanged: (val) {
-                length = int.parse(val);
-              },
-              style: const TextStyle(
-                  color: Color(0xFFFCF6BA),
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500),
-              decoration: InputDecoration(
-                counterText: "",
-                hintText: "Enter OTP Length.",
-                hintStyle: TextStyle(
-                    color: const Color(0xFFFCF6BA).withOpacity(0.35),
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400),
-                alignLabelWithHint: true,
-                border: const UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0xFFFCF6BA),
-                  ),
-                ),
-                focusedBorder: const UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0xFFFCF6BA),
-                    width: 1.5,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Buttons(
-            texts: "Generate OTP",
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: MediaQuery.of(context).size.height * 0.07,
-            onTap: () {
-              l1.clear();
-              setState(() {
-                if (length > 0) {
-                  for (int i = 0; i < length; i++) {
-                    l1.add(r1.nextInt(9).toString());
-                  }
-                } else {
-                  l1 = ["0", "0", "0", "0", "0", "0"];
-                  length = 0;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text("Enter Valid Length!"),
-                    ),
-                  );
-                }
-              });
-            },
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  Color(0xFFFFFFFF),
+                  Color(0xFFD8E6F4),
+                ])),
           ),
           Container(
-            child: Row(
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: l1
-                  .map((e) => Text(
-                        e,
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 30,
-                            fontWeight: FontWeight.w600),
-                      ))
-                  .toList(),
+              children: [
+                const Text(
+                  "Color Palette\nGenerator",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xFF3BB3F9),
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700),
+                ),
+                Column(
+                  children: List.generate(
+                      6,
+                      (index) => Container(
+                            height: MediaQuery.of(context).size.height * 0.09,
+                            width: MediaQuery.of(context).size.height * 0.15,
+                            decoration: BoxDecoration(
+                              color: colors[r1.nextInt(16)],
+                              borderRadius: radiusFinder(index),
+                            ),
+                          )),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: const Text(
+                    "Generate",
+                    style: TextStyle(
+                        color: Color(0xFF3BB3F9),
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Color(0xFF3BB3F9),
+                      width: 3,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
+              ],
             ),
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.height * 0.1,
-            decoration: BoxDecoration(
-              color: const Color(0xFFFFE8B8),
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          Buttons(
-            texts: "Reset",
-            width: MediaQuery.of(context).size.width * 0.45,
-            height: MediaQuery.of(context).size.height * 0.06,
-            onTap: () {
-              setState(() {
-                l1 = ["0", "0", "0", "0", "0", "0"];
-                lengthControler.clear();
-                length = 0;
-              });
-            },
           ),
         ],
       ),
-      backgroundColor: const Color(0xFF15172B),
     );
   }
 
-  Buttons({
-    required String texts,
-    width,
-    height,
-    onTap,
-  }) {
-    return InkWell(
-        onTap: onTap,
-        borderRadius: BorderRadius.circular(30),
-        child: Ink(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: const Color(0xFFFCF6BA).withOpacity(0.35),
-                blurRadius: 4,
-                spreadRadius: 2,
-                offset: const Offset(0, 2),
-              )
-            ],
-            borderRadius: BorderRadius.circular(15),
-            gradient: const LinearGradient(
-                colors: [Color(0xFFFFE8B8), Color(0xFFF6DB87)]),
-          ),
-          child: Container(
-            alignment: Alignment.center,
-            child: Text(
-              texts,
-              style: const TextStyle(
-                  color: Color(0xFF15172B),
-                  fontSize: 28,
-                  fontWeight: FontWeight.w500),
-            ),
-            width: width,
-            height: height,
-          ),
-        ));
+  radiusFinder(int n) {
+    if (n == 0) {
+      return const BorderRadius.only(
+          topRight: Radius.circular(20), topLeft: Radius.circular(20));
+    } else if (n == 5) {
+      return const BorderRadius.only(
+          bottomRight: Radius.circular(20), bottomLeft: Radius.circular(20));
+    } else {
+      BorderRadius.circular(0);
+    }
   }
 }
